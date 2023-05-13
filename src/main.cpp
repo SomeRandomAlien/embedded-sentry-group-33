b996efe351daeed76ee96e74638186f3d300d55b
Tommy Martinez <UGFruT@gmail.com>
2023-05-13 05:43:59 -0400

    Made changes to classes and added two functions for comparison at the end

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@
 #include <vector>
 #include <string>
 #include <cmath>
+using namespace std;
 
 // Starter Code Provided by the March 27 Recitation.
 SPI spi(PF_9, PF_8, PF_7,PC_1,use_gpio_ssel); // mosi, miso, sclk, cs
@@ -33,8 +34,6 @@ SPI spi(PF_9, PF_8, PF_7,PC_1,use_gpio_ssel); // mosi, miso, sclk, cs
 #define DATA_READY_FLAG 2
 #define SCALING_FACTOR 17.5f*0.017453292519943295769236907684886f / 1000.0f
 
-// Page 22 of STM32F429 User Manual
-InterruptIn int2(PA_2,PullDown);
 // Page 18 of the User Manual
 DigitalOut l1(LED1), l2(LED2);
 
@@ -48,34 +47,86 @@ TS_StateTypeDef ts;
 
 /*
 For this problem, we mainly use the UserButton (PA_0) as the trigger to our mode change,
-  Here are the possible mode for this problem:
-    0 - Default mode, nothing happens
-    1 - After the first press of the UserButton. It will start the Lock recording mode.
-    2 - After the second press of the UserButton. It will shut down the Lock recording 
-        and save it.
-    3 - After the third press of the UserButton. It will start the Unlock recording mode.
-    4 - After the fourth press of the UserButton. It will shut down the Unlock recording
-        and save it. Besides the recording, this part will also do the comparison of 
-        two saved data.
+Here are the possible mode for this problem, numbers correspond to presses of the UserButton:
+    0 - Initial state, nothing happens
+    1 - Starts lock recording mode.
+    2 - Should save the lock we just recorded. Nothing else happens
+    3 - We will attempt to unlock the resource.
+    4 - Compare the lock and unlock motions to determine if its fine
+     to unlock the resource.
+    5 - Determines if resource unlocks or stays locked.
+        User gets two choices:
+        - First choice: Press the UserButton again, reset everything
+          and start from stage 0. (Reset)
 
-    5 - After the fifth press of the UserButton. It will return the result of the 
-        comparison. Then there are two choices for the next step:
-          First choice: If we press the UserButton again, we will reset the whole process
-          and start from the Stage-0, which is the default mode. (Reset)
-
-          Second choice: If we press the touch screen, it will turn Stage-2, which allow
-          us to record the Unlock again and try one more time. (Retry)
+        - Second choice: Touch on the screen, returns to stage 2.
+          We also wipe the unlock vector (Retry)
 */
-volatile int RecordMode = 0; //Set up the int variable for recording mode
+volatile int buttonPresses = 0; //Set up the int variable for counting button presses
 
 // We will define a structure or a class here for the gyroscope data.
-class GyroscopeReading {
-  private:
-    float x;
-    float y;
-    float z;
+class GyroReading {
   public:
+    float axis_x;
+    float axis_y;
+    float axis_z;
     // Functions and definitions go here
+    GyroReading(float x, float y, float z) {
+      axis_x = x;
+      axis_y = y;
+      axis_z = z;
+    }
+
+    void printCoords() {
+      cout << "x_axis: " << axis_x << ", y_axis: " << axis_y << ", z_axis: " << axis_z << endl;
+    }
+};
+
+GyroReading readMovement() {
+  int16_t raw_gx,raw_gy,raw_gz;
+  float gx, gy, gz;
+
+  //wait until new sample is ready
+  flags.wait_all(DATA_READY_FLAG);
+  //prepare the write buffer to trigger a sequential read
+  write_buf[0]=OUT_X_L|0x80|0x40;
+
+  //start sequential sample reading
+  spi.transfer(write_buf,7,read_buf,8,spi_cb,SPI_EVENT_COMPLETE );
+  flags.wait_all(SPI_FLAG);
+
+  //read_buf after transfer: garbage byte, gx_low,gx_high,gy_low,gy_high,gz_low,gz_high
+  //Put the high and low bytes in the correct order lowB,Highb -> HighB,LowB
+  raw_gx=( ( (uint16_t)read_buf[2] ) <<8 ) | ( (uint16_t)read_buf[1] );
+  raw_gy=( ( (uint16_t)read_buf[4] ) <<8 ) | ( (uint16_t)read_buf[3] );
+  raw_gz=( ( (uint16_t)read_buf[6] ) <<8 ) | ( (uint16_t)read_buf[5] );
+
+  //printf("RAW|\tgx: %d \t gy: %d \t gz: %d\n",raw_gx,raw_gy,raw_gz);
+
+  gx=((float)raw_gx)*(SCALING_FACTOR);
+  gy=((float)raw_gy)*(SCALING_FACTOR);
+  gz=((float)raw_gz)*(SCALING_FACTOR);
+
+  //printf("Actual|\tgx: %4.5f \t gy: %4.5f \t gz: %4.5f\n",gx,gy,gz);
+  GyroReading current_reading(gy, gy, gz);
+  return current_reading;
+}
+
+class GyroRecording {
+public:
+  vector<GyroReading> gyroSample;
+
+  void recordMovement(const GyroReading& reading) {
+    gyroSample.push_back(reading);
+  }
+
+  int sampleSize() const {
+    return gyroSample.size();
+  }
+
+  GyroReading getGyroReadSample(int index) const {
+    return gyroSample[index];
+  }
 };
 
 EventFlags flags;
@@ -85,48 +136,99 @@ void spi_cb(int event){
   flags.set(SPI_FLAG);
 };
 
-// TODO: An “enter key” and ”record” functionality must be developed.
 void data_cb(){
   flags.set(DATA_READY_FLAG);
 };
 
 // TODO: Change this to a switch statement
 //This function is designed to switch the mode and visual LED signal (for UserButton):
-void Switch(){
-  if(RecordMode == 0 || RecordMode == 1){
+void Switch() {
+  if(buttonPresses == 0 || buttonPresses == 1){
     l1 = !l1; //Switch the LED signal for Lock Recording
-    RecordMode += 1; //Switch the Recording mode
-  }else if(RecordMode == 2 || RecordMode == 3){
+    buttonPresses += 1; //Switch the Recording mode
+  }else if(buttonPresses == 2 || buttonPresses == 3){
     l2 = !l2; //Switch the LED signal for Unlock Recording
-    RecordMode += 1; //Switch the Recording mode
-  }else if(RecordMode == 4){
+    buttonPresses += 1; //Switch the Recording mode
+  }else if(buttonPresses == 4){
     //Set all LEDs up as a signal of result
     l1 = 1;
     l2 = 1;
 
-    RecordMode += 1;  //Switch the Recording mode
+    buttonPresses += 1;  //Switch the Recording mode
   }else{
     //Reset all LEDs
     l1 = 0;
     l2 = 0;
 
     //Reset record mode
-    RecordMode = 0;
+    buttonPresses = 0;
   }
 }
 
 //This function will be used to avoid mis-read of Screen Touch
-bool TouchEnding(){
-  TS_StateTypeDef Now;          //Define a new TouchScreen State for further check
-  thread_sleep_for(100);        //Leaving a period time to allow finger move away
-  BSP_TS_GetState(&Now);        //Update the TouchScreen State
-  return !Now.TouchDetected;    //If now the touch ends, it will return true(bool).
+bool screenTouched(){
+  TS_StateTypeDef currentTouch;           //Define a new TouchScreen State for further check
+  thread_sleep_for(100);                  //Leaving a period time to allow finger move away
+  BSP_TS_GetState(&currentTouch);         //Update the TouchScreen State
+  return !currentTouch.TouchDetected;     //If screen is touched, it will return true
+}
+
+/*
+To calculate distance between 2 points in 3D, we used this resource:
+https://byjus.com/maths/distance-between-two-points-3d/
+
+ sqrt[(x2 – x1)^2 + (y2 – y1)^2 + (z2 – z1)^2]
+*/
+float distanceCalc(const GyroReading& currentSample, const GyroReading& previousSample) {
+  float comp_xaxis = pow(currentSample.axis_x - previousSample.axis_x, 2);
+  float comp_yaxis = pow(currentSample.axis_y - previousSample.axis_y, 2);
+  float comp_zaxis = pow(currentSample.axis_z - previousSample.axis_z, 2);
+
+  float dist_difference = sqrt(comp_xaxis + comp_yaxis + comp_zaxis);
+
+  return dist_difference;
+}
+
+/*
+We must use two vectors that we just finished sampling, and compare the two.
+As there will invariably be differences when it comes to size and samples, we must:
+
+- Take into account the limitations of the smaller set.
+- Compare the values of the axes to determine whether the resource can be unlocked.
+*/
+float similarityComp(const GyroRecording& largerSet, const GyroRecording& smallerSet) {
+  int smallerSetAmount = smallerSet.sampleSize();
+
+  float similarity = 0.00;
+  /*
+  We'll be starting from index 1, as the function (distanceCalc) above will calculate the distance
+  between one point and the previous one. Since Vectors such as the GyroRecording class start at
+  an index of 0, this should be fine. We should then compare the larger and smaller sets to see
+  how similar they are via division.
+  We should ideally get values close to 1 when dividing. Then we add our value we used division with
+  into the similarityVal above.
+  */
+  for (int i = 1; i <= smallerSetAmount; i++) {
+    GyroReading smlSetSmpCrnt = smallerSet.getGyroReadSample(i);
+    GyroReading smlSetSmpPrv = smallerSet.getGyroReadSample(i-1);
+    float smlSetSmpDist = distanceCalc(smlSetSmpCrnt, smlSetSmpPrv);
+
+    GyroReading lrgSetSmpCrnt = largerSet.getGyroReadSample(i);
+    GyroReading lrgSetSmpPrv = largerSet.getGyroReadSample(i-1);
+    float lrgSetSmpDist = distanceCalc(lrgSetSmpCrnt, lrgSetSmpPrv);
+
+    float currSimilarity = lrgSetSmpDist/smlSetSmpDist;
+    similarity += currSimilarity;
+  }
+
+  float finalSimilarity = similarity/smallerSetAmount;
+
+  return finalSimilarity;
 }
  
 int main() {
-
+  // Enable the button interrupt
   btn.rise(&Switch);
-  // Everything below will need to be in a new function
 
   // Setup the spi for 8 bit data, high steady state clock,
   // second edge capture, with a 1MHz clock rate
@@ -143,10 +245,6 @@ int main() {
   spi.transfer(write_buf,2,read_buf,2,spi_cb,SPI_EVENT_COMPLETE );
   flags.wait_all(SPI_FLAG);
 
-  //configure the interrupt to call our function
-  //when the pin becomes high
-  int2.rise(&data_cb);
-
   // Testing LCD Output
   lcd.Clear(LCD_COLOR_RED);
   lcd.DisplayStringAtLine(0, (uint8_t*)"Hello World!");
@@ -156,65 +254,25 @@ int main() {
   spi.transfer(write_buf,2,read_buf,2,spi_cb,SPI_EVENT_COMPLETE );
   flags.wait_all(SPI_FLAG);
 
-  // The gyroscope sensor keeps its configuration between power cycles.
-  // This means that the gyroscope will already have it's data-ready interrupt
-  // configured when we turn the board on the second time. This can lead to
-  // the pin level rising before we have configured our interrupt handler.
-  // To account for this, we manually check the signal and set the flag
-  // for the first sample.
-  if(!(flags.get()&DATA_READY_FLAG)&&(int2.read()==1)){
-    flags.set(DATA_READY_FLAG);
-  }
-  while (1) {
-    int16_t raw_gx,raw_gy,raw_gz;
-    float gx, gy, gz;
-
-    //wait until new sample is ready
-    flags.wait_all(DATA_READY_FLAG);
-    //prepare the write buffer to trigger a sequential read
-    write_buf[0]=OUT_X_L|0x80|0x40;
-
-    //start sequential sample reading
-    spi.transfer(write_buf,7,read_buf,8,spi_cb,SPI_EVENT_COMPLETE );
-    flags.wait_all(SPI_FLAG);
-
-    //read_buf after transfer: garbage byte, gx_low,gx_high,gy_low,gy_high,gz_low,gz_high
-    //Put the high and low bytes in the correct order lowB,Highb -> HighB,LowB
-    raw_gx=( ( (uint16_t)read_buf[2] ) <<8 ) | ( (uint16_t)read_buf[1] );
-    raw_gy=( ( (uint16_t)read_buf[4] ) <<8 ) | ( (uint16_t)read_buf[3] );
-    raw_gz=( ( (uint16_t)read_buf[6] ) <<8 ) | ( (uint16_t)read_buf[5] );
-
-    //printf("RAW|\tgx: %d \t gy: %d \t gz: %d\n",raw_gx,raw_gy,raw_gz);
-
-    gx=((float)raw_gx)*(SCALING_FACTOR);
-    gy=((float)raw_gy)*(SCALING_FACTOR);
-    gz=((float)raw_gz)*(SCALING_FACTOR);
-    
-    printf("Actual|\tgx: %4.5f \t gy: %4.5f \t gz: %4.5f\n",gx,gy,gz);
-
-  }
-
   // This will serve as our new main function, above will need to be put
   // into a new function
 
-  /*
   while(1) {
-    BSP_TS_GetState(&ts); //Get the TouchScreen status first
+    BSP_TS_GetState(&ts); //Get the TouchScreen status
 
-    if (RecordMode == 5 && ts.TouchDetected && TouchEnding()) {//Retry setting
-      RecordMode = 2;
+    if (buttonPresses == 5 && ts.TouchDetected && screenTouched()) {//Retry setting
+      buttonPresses = 2;
       //Clear all saved data in Unlock recording part (leave Lock alone!)
     }
 
-    if(RecordMode == 1){  //Start recording of Lock 
+    if(buttonPresses == 1){  //Start recording of Lock 
       //Save gyro data
-    }else if(RecordMode == 3){  //Start recording of Unlock
+    }else if(buttonPresses == 3){  //Start recording of Unlock
       //Save gyro data
-    }else if(RecordMode == 4){  //Start recording comparison
+    }else if(buttonPresses == 4){  //Start recording comparison
       //Calculation code
-    }else if(RecordMode == 5){  //Result return
+    }else if(buttonPresses == 5){  //Result return
       
     }
   }
-  */
 }
